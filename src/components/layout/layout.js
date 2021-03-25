21b5a94d061ec1f3842229579b356d9b43d261e4
Marysha <n01355798@humbermail.ca>
2021-03-24 21:16:24 -0400

    go to the top button

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
+import useScrollTrigger from "@material-ui/core/useScrollTrigger"
 
 import Header from "../header/header"
 import Footer from "../footer/footer"
@@ -10,6 +10,34 @@ import CssBaseline from "@material-ui/core/CssBaseline"
 import theme from "../theme"
 
 import "./layout.css"
+
+import { Fab } from "gatsby-theme-material-ui"
+import UpIcon from "@material-ui/icons/KeyboardArrowUp"
+import { makeStyles } from "@material-ui/core/styles"
+import { Grow } from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+  skipLink: {
+    position: "fixed",
+    bottom: theme.spacing(1),
+    right: theme.spacing(1),
+  },
+}))
+
+function ShowFabOnScroll(props) {
+  //threshold should be different for different media 200 - for less them sm, 600 starting md
+  // also make it smooth
+  const classes = useStyles()
+  const trigger = useScrollTrigger({ threshold: 600 })
+
+  return (
+    <Grow in={trigger} timeout={1000}>
+      <Fab to="#" color="secondary" size="small" className={classes.skipLink}>
+        <UpIcon />
+      </Fab>
+    </Grow>
+  )
+}
 
 const Layout = props => {
   return (
@@ -21,6 +49,10 @@ const Layout = props => {
         <div>
           <main>{props.children}</main>
         </div>
+        <div>
+          <ShowFabOnScroll />
+        </div>
+
         {/* <Footer /> */}
       </MuiThemeProvider>
     </>
