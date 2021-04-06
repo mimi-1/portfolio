402f26236ed274f2cfe579d8e077af584674e109
Marysha <n01355798@humbermail.ca>
2021-04-06 16:10:33 -0400

    some fields added to subscribe from

--- src/components/subscribe/subscribe.js
+++ src/components/subscribe/subscribe.js
@@ -15,31 +15,76 @@ const Subscribe = () => {
   //     cleanup
   //   }
   // }, [input])
+
   const [email, setEmail] = useState("")
+  const [listFields, setListFields] = useState({ FNAME: "Unknown", LNAME: "" })
   const [result, setResult] = useState({})
 
-  const handleSubmit = async e => {
-    const resp = await addToMailchimp(email)
-    setResult(resp)
-    console.log("Resuls is:", resp)
+  const handleSubmit = e => {
+    console.log("HANDLE SUBMIT FORM")
+    addToMailchimp(email, listFields)
+      .then(data => {
+        // I recommend setting data to React state
+        // but you can do whatever you want (including ignoring this `then()` altogether)
+        console.log(data)
+      })
+      .catch(() => {
+        // unnecessary because Mailchimp only ever
+        // returns a 200 status code
+        // see below for how to handle errors
+      })
+
+    // const resp = await addToMailchimp(email)
+    // setResult(resp)
+    // console.log("Resuls is:", resp)
   }
 
-  const handleChange = event => {
+  const handleEmailChange = event => {
     setEmail(event.target.value)
+  }
+
+  const handleFieldsChange = event => {
+    console.log(event.target.value)
+    console.log(event.target.name)
+    const value = event.target.value
+    const name = event.target.name
+    const newListFileds = { ...listFields, ...{ name: value } }
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <TextField
+        id="FNAME"
+        label="First Name"
+        type="text"
+        name="FNAME"
+        variant="outlined"
+        onChange={handleFieldsChange}
+        // value={listFields.FNAME}
+        error={false}
+      />
+
+      <TextField
+        id="LNAME"
+        label="Last Name"
+        type="text"
+        name="LNAME"
+        variant="outlined"
+        onChange={handleFieldsChange}
+      />
+
+      <TextField
         id="outlined-email-input"
+        required
         label="Email"
         type="email"
         name="email"
         autoComplete="email"
         variant="outlined"
-        onChange={handleChange}
+        onChange={handleEmailChange}
+        helperText="Email is required field"
       />
-      <br />
+
       <Button variant="contained" color="primary" label="Submit" type="submit">
         <Typography variant="button">Subscribe</Typography>
       </Button>
